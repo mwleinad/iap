9de067ff5562c2fff55a36402aa783754fcf9ff6
root <root@eg.server1.iapchiapas.org.mx>
2017-06-15 13:09:11 -0400

    cambios despues de revision

--- index.php
+++ index.php
@@ -203,7 +203,8 @@ $smarty->assign('timestamp', time());
 ini_set("display_errors", "ON"); 
 $showErrors = "E_ALL";
 error_reporting($showErrors);
-if($includedTpl == 'login'){
+
+if($includedTpl == 'login'){ 
 	$smarty->display(DOC_ROOT.'/templates/login_new.tpl');
 }
 else
