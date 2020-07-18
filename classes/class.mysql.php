102159c500e564cf1cf46ed90185b2a479117e43
mwleinad <dlopez@trazzos.com>
2020-07-18 15:21:10 -0500

    mysql

--- classes/class.mysql.php
+++ classes/class.mysql.php
@@ -1,46 +1,46 @@
 <?php
 class MySQL
 {
-  var $conexion;
-  function MySQL()
-  {
-  	if(!isset($this->conexion))
-	{
-  		$this->conexion = (mysql_connect(SQL_HOST,SQL_USER,SQL_PASSWORD)) or die(mysql_error());
-  		mysql_select_db(SQL_DATABASE,$this->conexion) or die(mysql_error());
-  	}
-  }
+    var $conexion;
+    function MySQL()
+    {
+        if(!isset($this->conexion))
+        {
+            $this->conexion = mysqli_connect(SQL_HOST, SQL_USER, SQL_PASSWORD, SQL_DATABASE);
+        }
+    }
 
- function consulta($consulta)
- {
-	$resultado = mysql_query($consulta,$this->conexion);
-  	if(!$resultado)
-	{
-  		echo 'MySQL Error: ' . mysql_error();
-	    exit;
-	}
-  	return $resultado; 
-  }
-  
- function fetch_array($consulta)
- { 
-  	return mysql_fetch_array($consulta);
- }
- 
- function num_rows($consulta)
- { 
- 	 return mysql_num_rows($consulta);
- }
- 
- function fetch_row($consulta)
- { 
- 	 return mysql_fetch_row($consulta);
- }
- function fetch_assoc($consulta)
- { 
- 	 return mysql_fetch_assoc($consulta);
- } 
- 
+    function consulta($consulta)
+    {
+        mysqli_query($this->conexion, "SET NAMES 'utf8'");
+        $resultado = mysqli_query($this->conexion, $consulta);
+        if(!$resultado)
+        {
+            echo 'MySQL Error: ' . mysqli_error();
+            exit;
+        }
+        return $resultado;
+    }
+
+    function fetch_array($consulta)
+    {
+        return mysqli_fetch_array($consulta);
+    }
+
+    function num_rows($consulta)
+    {
+        return mysqli_num_rows($consulta);
+    }
+
+    function fetch_row($consulta)
+    {
+        return mysqli_fetch_row($consulta);
+    }
+    function fetch_assoc($consulta)
+    {
+        return mysqli_fetch_assoc($consulta);
+    }
+
 }
 
 ?>
