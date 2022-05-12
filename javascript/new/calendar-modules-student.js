97c7ee7ec5d1117d5fe48795e14dc801ba4e3fc6
Carlos Zenteno <carloszh04@gmail.com>
2022-05-12 11:32:38 -0500

    Correccion Actividades - Examenes

--- javascript/new/calendar-modules-student.js
+++ javascript/new/calendar-modules-student.js
@@ -44,3 +44,20 @@ function cancelarSolicitud(){
 	$("#ajax").modal("hide");
 	
 }
+
+function DoTest(id)
+{
+    Swal.fire({
+        title: '¿Esta seguro de que desea presentar este examen?',
+        text: 'El tiempo empezara a correr despues de aceptar',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#7ed264',
+        cancelButtonColor: '#ff4545',
+        confirmButtonText: 'Confirmar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+            window.location = WEB_ROOT+"/make-test/id/"+id;
+        }
+    });
+}
