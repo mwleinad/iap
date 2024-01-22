35e19425c4b53cca2ced539526f5634b25523687
William Ramírez <wramirez@iapchiapas.edu.mx>
2024-01-22 14:45:39 -0600

    Escapando comillas simples al agregar la actividad

--- modules/add-activity.php
+++ modules/add-activity.php
@@ -39,7 +39,7 @@
 		$activity->setHora($_POST["hora"]);
 		$activity->setModality($_POST["modality"]);
 		$activity->setResumen($_POST["resumen"]);
-		$activity->setDescription($_POST["description"]);
+		$activity->setDescription(addslashes($_POST["description"]));
 		$activity->setRequiredActivity($_POST["requiredActivity"]);
 		$activity->setPonderation($_POST["ponderation"]);
 		$activity->setHoraInicial($_POST["horaInicial"]);
