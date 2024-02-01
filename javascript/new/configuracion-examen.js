17bf96a9c1c7d0a18c45dab0c3c549528a1e8104
William Ramírez <wramirez@iapchiapas.edu.mx>
2024-02-01 15:47:02 -0600

    Se cierra el modal al actualizar el examen

--- javascript/new/configuracion-examen.js
+++ javascript/new/configuracion-examen.js
@@ -1,34 +1,28 @@
-function EditTest(){
-	
+function EditTest() {
+
 	// alert(WEB_ROOT)
 	$.ajax({
-	  	type: "POST",
-	  	url: WEB_ROOT+'/ajax/configuracion-examen.php',
-	  	// data: "type=EditTest&id="+id,
-		data: $("#editMajorForm").serialize(true)+'&type=EditTest&activityId='+$("#activityId").val(),
-		beforeSend: function(){			
+		type: "POST",
+		url: WEB_ROOT + '/ajax/configuracion-examen.php',
+		// data: "type=EditTest&id="+id,
+		data: $("#editMajorForm").serialize(true) + '&type=EditTest&activityId=' + $("#activityId").val(),
+		beforeSend: function () {
 			$("#msj").html(LOADER3);
 		},
-	  	success: function(response) {	
-				console.log(response)
+		success: function (response) { 
+			$("#msj").html('');
+			var splitResponse = response.split("[#]"); 
+			if ($.trim(splitResponse[0]) == "ok") { 
+				$("#ajax").modal("hide");
+				if ($('.modal-backdrop').is(':visible')) {
+					$('body').removeClass('modal-open');
+					$('.modal-backdrop').remove();
+				};
+			} else {
+				$("#msj").html(splitResponse[1]);
+			} 
+		} 
+	});
 
-			  $("#msj").html('');
-			 var splitResponse = response.split("[#]");
 
-			 if($.trim(splitResponse[0])=="ok"){
-
-				 $("#tblContent2").html(splitResponse[1]);
-				 btnClose();
-			 }else{
-				 $("#msj").html(splitResponse[1]);
-			 }
-
-
-		},
-		error:function(){
-			// alert(msgError);
-		}
-    });
-
-	
 }//AddReg
