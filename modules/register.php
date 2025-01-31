897096ffc0e1978b09d03bba9470c573219b591d
William Ramírez <wramirez@iapchiapas.edu.mx>
2025-01-31 11:59:43 -0600

    Se agregaron acreditaciónes como tipo de currícula

--- modules/register.php
+++ modules/register.php
@@ -2,7 +2,7 @@
 $student->setCountry(1);
 $estados = $student->EnumerateEstados();
 
-$where = "AND major.majorId IN(1, 18, 34) AND subject.listarRegistro = 1 ORDER BY major.majorId, subject.name";
+$where = "AND major.majorId IN(1, 18, 34, 35, 37) AND subject.listarRegistro = 1 ORDER BY major.majorId, subject.name";
 $subjects = $subject->getSubjects($where);
 
 $smarty->assign('curriculas', $subjects);
